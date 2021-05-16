a28794b5fdc241a9ab3a555108e0846df0e9da13
Your Name <you@example.com>
2021-05-16 18:22:50 +0300

    added dates

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   Quotes = [
-    new Quote(1, 'Life is like a river', 'Despite everything, keep moving'),
-    new Quote(2, 'River is like a tree', 'life giving as you and I were meant to be'),
-    new Quote(3, 'Tree is like wind', 'break speed of those attacking youre way, but also uproot those holding you up from your destiny'),
-    new Quote(4, 'Wind is like a mountain','Too big, to high,too cold. They will never figure you'),
-    new Quote(5, 'Mountain is like an ocean', 'Think vast, Beyond human understanding'),
-    new Quote(6, 'Oceans are fed by rivers','Everything is connected'),
+    new Quote(1, 'Life is like a river', 'Despite everything, keep moving', new Date(2018,2,3)),
+    new Quote(2, 'River is like a tree', 'life giving as you and I were meant to be',new Date(2018,2,3)),
+    new Quote(3, 'Tree is like wind', 'break speed of those attacking youre way, but also uproot those holding you up from your destiny',new Date(2018,2,3)),
+    new Quote(4, 'Wind is like a mountain','Too big, to high,too cold. They will never figure you', new Date(2018,2,3)),
+    new Quote(5, 'Mountain is like an ocean', 'Think vast, Beyond human understanding',new Date(2018,2,3)),
+    new Quote(6, 'Oceans are fed by rivers','Everything is connected',new Date(2018,2,3)),
   ]
   toggleDetails(index){
     this.Quotes[index].showDescription=!this.Quotes[index].showDescription;
