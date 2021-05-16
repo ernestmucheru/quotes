9389bf1095614b735b2c1acf0412cb9a1382880d
Your Name <you@example.com>
2021-05-16 17:55:32 +0300

    create toggle description buttn and assign function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,13 +9,16 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   Quotes = [
-    new Quote(1, 'Life is like a river', 'Despite everything, it keeps moving'),
-    new Quote(2, 'River is like a tree', 'life giving in its nature'),
-    new Quote(3, 'Tree is like wind', 'knows all our secrets,whispers them to nature'),
-    new Quote(4, 'Wind is like a mountain','too big, to high,too cold to catch up'),
-    new Quote(5, 'Mountain is like an ocean', 'vast like our thoughts. Beyond human understanding'),
-    new Quote(6, 'Oceans are fed by rivers','everything is connected'),
+    new Quote(1, 'Life is like a river', 'Despite everything, keep moving'),
+    new Quote(2, 'River is like a tree', 'life giving as you and I were meant to be'),
+    new Quote(3, 'Tree is like wind', 'break speed of those attacking youre way, but also uproot those holding you up from your destiny'),
+    new Quote(4, 'Wind is like a mountain','Too big, to high,too cold. They will never figure you'),
+    new Quote(5, 'Mountain is like an ocean', 'Think vast, Beyond human understanding'),
+    new Quote(6, 'Oceans are fed by rivers','Everything is connected'),
   ]
+  toggleDetails(index){
+    this.Quotes[index].showDescription=!this.Quotes[index].showDescription;
+  }
   constructor() { }
   ngOnInit(): void {
   }
